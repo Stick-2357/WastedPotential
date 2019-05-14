14554b0a9b2079f4efa4ab36f00c86d0f8338c39
emonnig738 <emonnig738@STA-LAB-SD2-17.LSR7SD.ORG>
2019-05-14 08:50:24 -0500

    initial setup of the test file, w/o GUI

---
+++ WastedPotentialProject/src/wastedpotentialproject/WastedPotentialProject.java
@@ -0,0 +1,11 @@
+/*     Team: Wasted Potential
+    Project: ProDeo Router
+Description: Takes mulitple addresses, and finds quickest route
+*/
+package wastedpotentialproject;
+
+public class WastedPotentialProject {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
