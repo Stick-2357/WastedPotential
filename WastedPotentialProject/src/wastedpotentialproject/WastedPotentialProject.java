8ec67f6e2e5533a9e68283e9132d33dcb111d2f1
emonnig738 <emonnig738@STA-LAB-SD2-17.LSR7SD.ORG>
2019-05-21 08:17:17 -0500

    Pulls addresses from DB

--- WastedPotentialProject/src/wastedpotentialproject/WastedPotentialProject.java
+++ WastedPotentialProject/src/wastedpotentialproject/WastedPotentialProject.java
@@ -11,40 +11,53 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class WastedPotentialProject {
+    final static String SELECT_QUERY = "SELECT * FROM address";
 
     /**
      * @param args the command line arguments
      * @throws java.net.MalformedURLException
      */
-    public static void main(String[] args) throws MalformedURLException, IOException {
+    public static void main(String[] args) throws MalformedURLException, IOException, ClassNotFoundException, SQLException {
         String genericURL = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=2D8iqF8UC43KY06X3cHIPWDoTGP1AR9d&from=";
         ArrayList<String> addresses = new ArrayList<>();
         //adds addresses to arraylist
-        //TODO read addresses from DB
-        addresses.add("1101+NW+Innovation+Parkway,+Lee's+Summit,+MO");
-        addresses.add("555+SW+Trailpark+DR,+Lee's+Summit,+MO");
-        addresses.add("117+N+Eastglen+Dr,+Raymore,+MO");
-        addresses.add("2801+NE+McBaine+Dr,+Lee's+Summit,+MO");
-        addresses.add("554+SW+Trailpark+DR,+Lee's+Summit,+MO");
-        addresses.add("1306+Tyler+LN,+Greenwood,+MO");
-        //concats all of the addresses
-        String adressesString = "";  
-        for (String address : addresses) {
-            adressesString += address + "&to=";
-        }
-        adressesString += "\b\b\b\b ";
-        //makes URL
-        URL url = new URL(genericURL + adressesString);
-        // Get the input stream through URL Connection
-        URLConnection con = url.openConnection();
-        InputStream is =con.getInputStream();
+        //create our mysql database connection
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wastedpotential", "root", "yoast");
+        PreparedStatement st = con.prepareStatement(SELECT_QUERY);
+        ResultSet r1=st.executeQuery();
 
-        //reads from input stream
+        //iterate through the java resultset
+        while (r1.next()) {
+          int id = r1.getInt("idaddress");
+          String street = r1.getString("address");
+          String city = r1.getString("city");
+          String state = "MO";
+          int zip = r1.getInt("zipcode");
+
+          //add result to arraylist
+          String tempAddress = street + ",+" + city + ",+" + state + ",+" + zip;
+          addresses.add(tempAddress.replaceAll(" ", "\\+"));
+        }
+        st.close();
+        String startingAddress = "1101 NW Innovation Parkway, Lee's Summit, MO, 64086";
+        
+        //concats all of the addresses
+        String addressesString = startingAddress.replace(" ", "\\+") + "&to=";  
+        for (String address : addresses) {
+            addressesString += address + "&to=";
+        }
+        addressesString += "\b\b\b\b ";
+
+        //get the input stream from the URL, and reads it        
+        URL url = new URL(genericURL + addressesString);
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        // read order, and return addresses in that order
+        //read order, and return addresses in that order
         String line;
         while ((line = br.readLine()) != null) {
             int indexOf = (line.indexOf("locationSequence") + 19);
@@ -52,11 +65,12 @@ public class WastedPotentialProject {
             String[] orderOfAddressIndexs = order.split(",");
             ArrayList<String> orderOfAddresses = new ArrayList<>();
             
-            for (String address : orderOfAddressIndexs) {
-                orderOfAddresses.add(addresses.get(Integer.parseInt(address)));
+             for (int i = 1; i < orderOfAddressIndexs.length; i++) {
+                orderOfAddresses.add(addresses.get(Integer.parseInt(orderOfAddressIndexs[i]) - 1));
             }
+            System.out.println(startingAddress);
             for (String address : orderOfAddresses) {
-                System.out.println(address.replaceAll("\\+", " "));
+                    System.out.println(address.replaceAll("\\+", " "));
             }
         }
     }
